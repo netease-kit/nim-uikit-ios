2c3c28ea65a98ccfea088f9c2b58c2d000d2d6a0
chris.ray <chris__w@163.com>
2015-11-01 15:30:25 +0800

    add custom cell guide code

---
+++ NIMKitDemo/DemoApplication/DemoApplication/CustomChatCell/Message/AttachmentDecoder.h
@@ -0,0 +1,15 @@
+//
+//  AttachmentDecoder.h
+//  DemoApplication
+//
+//  Created by chris on 15/11/1.
+//  Copyright © 2015年 chris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NIMSDK.h"
+
+@interface AttachmentDecoder : NSObject<NIMCustomAttachmentCoding>
+
+
+@end
---
+++ NIMKitDemo/DemoApplication/DemoApplication/CustomChatCell/SessionConfig.h
@@ -0,0 +1,14 @@
+//
+//  SessionConfig.h
+//  DemoApplication
+//
+//  Created by chris on 15/11/1.
+//  Copyright © 2015年 chris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NIMKit.h"
+
+@interface SessionConfig : NSObject<NIMSessionConfig>
+
+@end
