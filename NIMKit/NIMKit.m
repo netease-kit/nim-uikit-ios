a5db0763973e233473b8693a1ac332d855a7b3dd
chris.ray <chris__w@163.com>
2017-04-20 16:17:07 +0800

    auto commit version 3.6.0

---
+++ NIMKit/NIMKit.m
@@ -0,0 +1,153 @@
+//
+//  NIMKit.m
+//  NIMKit
+//
+//  Created by amao on 8/14/15.
+//  Copyright (c) 2015 NetEase. All rights reserved.
+//
+
+#import "NIMKit.h"
+#import "NIMKitTimerHolder.h"
+#import "NIMKitNotificationFirer.h"
+#import "NIMKitDataProviderImpl.h"
+#import "NIMCellLayoutConfig.h"
+#import "NIMKitUIConfig.h"
+#import "NIMKitInfoFetchOption.h"
+
+extern NSString *const NIMKitUserInfoHasUpdatedNotification;
+extern NSString *const NIMKitTeamInfoHasUpdatedNotification;
+
+
+@interface NIMKit(){
+    NSRegularExpression *_urlRegex;
+}
+@property (nonatomic,strong)    NIMKitNotificationFirer *firer;
+@property (nonatomic,strong)    id<NIMCellLayoutConfig> layoutConfig;
+@end
+
+
+@implementation NIMKit
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _resourceBundleName  = @"NIMKitResource.bundle";
+        _emoticonBundleName  = @"NIMKitEmoticon.bundle";
+        _settingBundleName   = @"NIMKitSettings.bundle";
+        _firer = [[NIMKitNotificationFirer alloc] init];
+        _provider = [[NIMKitDataProviderImpl alloc] init];   //默认使用 NIMKit 的实现
+
+        _layoutConfig = [[NIMCellLayoutConfig alloc] init];
+    }
+    return self;
+}
+
++ (instancetype)sharedKit
+{
+    static NIMKit *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[NIMKit alloc] init];
+    });
+    return instance;
+}
+
+- (void)registerLayoutConfig:(Class)layoutConfigClass
+{
+    id instance = [[layoutConfigClass alloc] init];
+    if ([instance isKindOfClass:[NIMCellLayoutConfig class]])
+    {
+        self.layoutConfig = instance;
+    }
+    else
+    {
+        NSAssert(0, @"class should be subclass of NIMLayoutConfig");
+    }
+}
+
+- (id<NIMCellLayoutConfig>)layoutConfig
+{
+    return _layoutConfig;
+}
+
+- (void)notfiyUserInfoChanged:(NSArray *)userIds{
+    if (!userIds.count) {
+        return;
+    }
+    for (NSString *userId in userIds) {
+        NIMSession *session = [NIMSession session:userId type:NIMSessionTypeP2P];
+        NIMKitFirerInfo *info = [[NIMKitFirerInfo alloc] init];
+        info.session = session;
+        info.notificationName = NIMKitUserInfoHasUpdatedNotification;
+        [self.firer addFireInfo:info];
+    }
+}
+
+- (void)notifyTeamInfoChanged:(NSArray *)teamIds{
+    if (teamIds.count) {
+        for (NSString *teamId in teamIds) {
+            [self notifyTeam:teamId];
+        }
+    }else{
+        [self notifyTeam:nil];
+    }
+}
+
+- (void)notifyTeamMemebersChanged:(NSArray *)teamIds
+{
+    if (teamIds.count) {
+        for (NSString *teamId in teamIds) {
+            [self notifyTeamMemebers:teamId];
+        }
+    }else{
+        [self notifyTeamMemebers:nil];
+    }
+}
+
+
+- (void)notifyTeam:(NSString *)teamId
+{
+    NIMKitFirerInfo *info = [[NIMKitFirerInfo alloc] init];
+    if (teamId.length) {
+        NIMSession *session = [NIMSession session:teamId type:NIMSessionTypeTeam];
+        info.session = session;
+    }
+    info.notificationName = NIMKitTeamInfoHasUpdatedNotification;
+    [self.firer addFireInfo:info];
+}
+
+- (void)notifyTeamMemebers:(NSString *)teamId
+{
+    NIMKitFirerInfo *info = [[NIMKitFirerInfo alloc] init];
+    if (teamId.length) {
+        NIMSession *session = [NIMSession session:teamId type:NIMSessionTypeTeam];
+        info.session = session;
+    }
+    extern NSString *NIMKitTeamMembersHasUpdatedNotification;
+    info.notificationName = NIMKitTeamMembersHasUpdatedNotification;
+    [self.firer addFireInfo:info];
+}
+
+- (NIMKitInfo *)infoByUser:(NSString *)userId option:(NIMKitInfoFetchOption *)option
+{
+    NIMKitInfo *info = nil;
+    if (self.provider && [self.provider respondsToSelector:@selector(infoByUser:option:)]) {
+        info = [self.provider infoByUser:userId option:option];
+    }
+    return info;
+}
+
+- (NIMKitInfo *)infoByTeam:(NSString *)teamId option:(NIMKitInfoFetchOption *)option
+{
+    NIMKitInfo *info = nil;
+    if (self.provider && [self.provider respondsToSelector:@selector(infoByTeam:option:)]) {
+        info = [self.provider infoByTeam:teamId option:option];
+    }
+    return info;
+
+}
+
+
+@end
+
+
+
