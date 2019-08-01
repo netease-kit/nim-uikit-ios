fc6afdef8d48e1f1afc0b840b94622986d28cc79
Elf Sundae <elf.sundae@gmail.com>
2019-08-02 00:27:43 +0800

    Remove unnecessary FLAnimatedImageView.h imports

--- NIMKit/NIMKit/Classes/Global/NIMKitDependency.h
+++ NIMKit/NIMKit/Classes/Global/NIMKitDependency.h
@@ -26,10 +26,8 @@
 
 #if __has_include(<FLAnimatedImage/FLAnimatedImage.h>)
 #import <FLAnimatedImage/FLAnimatedImage.h>
-#import <FLAnimatedImage/FLAnimatedImageView.h>
 #elif __has_include("FLAnimatedImage.h")
 #import "FLAnimatedImage.h"
-#import "FLAnimatedImageView.h"
 #else
 @import FLAnimatedImage;
 #endif
